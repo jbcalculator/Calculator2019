54ea29a43db53571b4ac86b2fce4342dfa9957af
jb <46296962+jbcalculator@users.noreply.github.com>
2019-06-25 07:30:01 +0700

    Update level.js

--- One Piece BloxPiece/Achievement/script/level.js
+++ One Piece BloxPiece/Achievement/script/level.js
@@ -1,10 +1,9 @@
-function getLevel(money,clevel)
-{
-    
-    while (money > 0) {
-       
+function getLevel(money, clevel) {
 
-      
+    while (money > 0) {
+
+
+
         if (clevel >= 0 && clevel < 25 && money >= 1) {
             clevel += 1;
             money -= 1;
@@ -102,11 +101,145 @@ function getLevel(money,clevel)
             money -= 12.5;
             // return 12.5f;
         } else {
-           
+
             break;
         }
-      
-    
+
+
+    }
+    alert("Level :" + clevel + "\n" + "เงินทอน :" + money);
 }
-    alert("Level :" + clevel +"\n" + "เงินทอน :" + money);
+
+function getMoney(rlevel, clevel) {
+var money = 0;
+    while (rlevel > 0) {
+
+        if (clevel >= 0 && clevel < 25) {
+            clevel++;
+            rlevel--;
+            money+=1;
+            // return 1f;
+        } else if (clevel >= 25 && clevel < 50) {
+            clevel++;
+            rlevel--;
+            money+=1.5;
+            //  return 1.5f;
+        } else if (clevel >= 50 && clevel < 75) {
+            clevel++;
+            rlevel--;
+            money+=2;
+            // return 2f;
+        } else if (clevel >= 75 && clevel < 100) {
+            clevel++;
+            rlevel--;
+            money+=2.5;
+            // return 2.5f;
+        } else if (clevel >= 100 && clevel < 125) {
+            clevel++;
+            rlevel--;
+            money+=3;
+            // return 3f;
+        } else if (clevel >= 125 && clevel < 150) {
+            clevel++;
+            rlevel--;
+            money+=3.5;
+            // return 3.5f;
+        } else if (clevel >= 150 && clevel < 175) {
+            clevel++;
+            rlevel--;
+            money+=4;
+            // return 4f;
+        } else if (clevel >= 175 && clevel < 200) {
+            clevel++;
+            rlevel--;
+            money+=4.5;
+            // return 4.5f;
+        } else if (clevel >= 200 && clevel < 225) {
+            clevel++;
+            rlevel--;
+            money+=5;
+            // return 5f;
+        } else if (clevel >= 225 && clevel < 250) {
+            clevel++;
+            rlevel--;
+            money+=5.5;
+            // return 5.5f;
+        } else if (clevel >= 250 && clevel < 275) {
+            clevel++;
+            rlevel--;
+            money+=6;
+            // return 6f;
+        } else if (clevel >= 275 && clevel < 300) {
+            clevel++;
+            rlevel--;
+            money+=6.5;
+            // return 6.5f;
+        } else if (clevel >= 300 && clevel < 325) {
+            clevel++;
+            rlevel--;
+            money+=7;
+            // return 7f;
+        } else if (clevel >= 325 && clevel < 350) {
+            clevel++;
+            rlevel--;
+            money+=7.5;
+            // return 7.5f;
+        } else if (clevel >= 350 && clevel < 375) {
+            clevel++;
+            rlevel--;
+            money+=8;
+            // return 8f;
+        } else if (clevel >= 375 && clevel < 400) {
+            clevel++;
+            rlevel--;
+            money+=8.5;
+            // return 8.5f;
+        } else if (clevel >= 400 && clevel < 425) {
+            clevel++;
+            rlevel--;
+            money+=9;
+            // return 9f;
+        } else if (clevel >= 425 && clevel < 450) {
+            clevel++;
+            rlevel--;
+            money+=9.5;
+            // return 9.5f;
+        } else if (clevel >= 450 && clevel < 475) {
+            clevel++;
+            rlevel--;
+            money+=10;
+            // return 10f;
+        } else if (clevel >= 475 && clevel < 500) {
+            clevel++;
+            rlevel--;
+            money+=10.5;
+            //  return 10.5f;
+        } else if (clevel >= 500 && clevel < 525) {
+            clevel++;
+            rlevel--;
+            money+=11;
+            //  return 11f;
+        } else if (clevel >= 525 && clevel < 550) {
+            clevel++;
+            rlevel--;
+            money+=11.5;
+            // return 11.5f;
+        } else if (clevel >= 550 && clevel < 575) {
+            clevel++;
+            rlevel--;
+            money+=12;
+            // return 12f;
+        } else if (clevel >= 575 && clevel < 600) {
+            clevel++;
+            rlevel--;
+            money+=12.5;
+            // return 12.5f;
+        } else {
+
+            break;
+        }
+
+
+    }
+    alert("ค่าใช่จ่าย :" + money);
 }
